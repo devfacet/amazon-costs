b5f8a80b8be5599343dc041f40b3e94144f1c87a
Fatih Cetinkaya <cmuratfatih@gmail.com>
2013-08-12 23:00:39 -0400

    Tidy: Header info and copyright

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+/*
+ * Amazon Costs
+ * Copyright (c) 2013 Fatih Cetinkaya (http://github.com/cmfatih/amazon-costs)
+ * For the full copyright and license information, please view the LICENSE.txt file.
+ */
+
 // Init reqs
 module.exports = require('./lib/');
--- test/test-all.js
+++ test/test-all.js
@@ -1,3 +1,9 @@
+/*
+ * Amazon Costs
+ * Copyright (c) 2013 Fatih Cetinkaya (http://github.com/cmfatih/amazon-costs)
+ * For the full copyright and license information, please view the LICENSE.txt file.
+ */
+
 // Init reqs
 var mAmzCosts   = require('../');     // amazon-costs module
 
